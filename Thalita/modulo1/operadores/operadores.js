c021aef1902118f0683b9470c06ec92c89d94fe3
ThalitaCesar <thalita.walleska@gmail.com>
2021-11-11 12:08:54 -0300

    adcionando operadores.html

---
+++ Thalita/modulo1/operadores/operadores.js
@@ -0,0 +1,131 @@
+//1)
+
+/* const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado) //false
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) //false
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado) //true
+
+console.log("d. ", typeof resultado) //bolean
+
+//2.) e 3) // No código acima não especifica o número, realizando o calculo de string. O código correto seria:
+
+let PrimeiroNumero = Number(prompt("Dígite um numero:"))
+let SegundoNumero = Number(prompt("Digite um segundo numero:"))
+
+const soma1 = PrimeiroNumero + SegundoNumero 
+
+console.log(soma1) */
+
+//ESCRITA DE CÓDIGO
+
+//1)
+/* let nome = prompt("Qual seu nome?");
+let idadeMelhorAmigo = Number (prompt ("Qual o ano de nascimento do seu melhor amigo/a?"));
+let idadeUsuario = Number (prompt ("Qual o seu ano de nascimento?"));
+
+console.log("sua idade é maior do que seu melhor amigo?", idadeUsuario < idadeMelhorAmigo)
+
+//resposta (`true` ou `false`)
+
+console.log ("Você e seu amigo tem quantos anos de diferença?", idadeMelhorAmigo-idadeUsuario) */
+
+//2.)
+/*let numeroPar = Number (prompt("Insira um numero par:"))
+console.log (numeroPar % 2) */
+
+//quando se coloca um número par o resultado dá ), quando coloca um numero impar o resultado dá 1. 
+//3.)
+
+/* let qtdAnos = Number (prompt ("quantos anos você tem?"));
+let qtdMeses =12
+
+let qtdDias = 365
+let qdtHoras = 24
+console.log ("a.) Você tem os seguintes meses de vida:", qtdAnos*qtdMeses)
+console.log ("b.) Você tem os seguintes dias de vida:",qtdAnos * qtdDias)
+console.log ("c.) Você tem as seguintes horas de vida:", qtdAnos*qtsdias*qdtHoras) */
+
+//4.)
+/*let primeiroNumero = Number (prompt("Escreva um número:"))
+let segundoNumero = Number (prompt("Escreva um segundo número:"))
+
+console.log ("a) O primeiro número é maior que o segundo?", primeiroNumero > segundoNumero)
+console.log ("b) o primeiro numero é igual ao segundo numero?", primeiroNumero == segundoNumero)
+console.log ("c) o primeiro número é divisivel pelo segundo número? Se a respota for 0 é true, se for mais do que isso é false", primeiroNumero / segundoNumero)
+console.log ("d) o segundo número é divisivel pelo primeiro número? Se a respota for 0 é true, se for mais do que isso é false", segundoNumero / primeiroNumero)
+*/
+
+//DESAFIO 
+
+//a) 
+/*
+{
+const libra = 20
+const kilo = 0.453592
+
+console.log( "20 libras equivalem a", libra * kilo, "kg")
+}
+
+//b) 
+{
+const onca = 10.5
+const kilo = 0.56699
+
+console.log( "10.5oz equivalem a", onca * kilo, "kg")
+}
+
+//c) 
+{
+    const milhas = 100
+    const metros = 1609.34
+    
+    console.log( "100mi equivalem  a", milhas * metros, "m")
+}
+    
+//d) 
+{
+    const pe = 50
+    const metros = 0.3048
+    
+    console.log( "50ft equivalem a", pe* metros, "m")
+}
+
+//e) 
+{
+    const galaoAmericano = 103.56
+    const litro = 3.7854
+    
+    console.log( "103.56gal equivalem a", galaoAmericano* litro, "l")
+}
+
+//f) 
+{
+    const xicara = 450
+    const litro = 0.24
+    
+    console.log( "450 xic equivalem a", xicara* litro, "l")
+}
+
+//g)
+
+{
+    let xicaras = prompt ("Quantas xicaras você quer calcular para litros?")
+    let litro = 0.24
+    
+    console.log( "As xicaras equivalem a", xicaras*litro, "l")
+}
+
+*/
+
+// os numeros que continham (.) não calculou e as respotas ficaram com muitos numeros.
+
+
+
