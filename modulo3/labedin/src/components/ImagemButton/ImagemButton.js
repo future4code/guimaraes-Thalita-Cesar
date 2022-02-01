23fa5b019464c9349ee4852c96b9e36c6d746259
ThalitaCesar <thalita.walleska@gmail.com>
2022-02-01 20:23:28 -0300

    exercicio estados

--- modulo3/labedin/src/components/ImagemButton/ImagemButton.js
+++ modulo3/labedin/src/components/ImagemButton/ImagemButton.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './ImagemButton.css'
+import React from "react";
+import styled from "styled-components";
+
+const ImageButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 50px;
+  width: 200px;
+  padding: 15px 30px;
+  margin: 10px auto;
+  background-color: red; 
+`;
+
+const ButtonImage = styled.img`
+  width: 30px;
+  margin-right: 10px;
+`;
 
 function ImagemButton(props) {
-    return (
-        <div className="image-button-container">
-            <img src={ props.imagem }/>
-            <p>{ props.texto }</p>
-        </div>
-
-    )
+  return (
+    <ImageButtonContainer>
+      <ButtonImage src={props.imagem} />
+      <p>{props.texto}</p>
+    </ImageButtonContainer>
+  );
 }
 
 export default ImagemButton;
